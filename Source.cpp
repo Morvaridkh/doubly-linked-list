7d2943e598094060ebbe1db772115851cd8dbc0f
Morvaridkh <165634813+Morvaridkh@users.noreply.github.com>
2025-01-19 09:47:34 +0330

    Add files via upload
    last commit

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 //Enter data
-
 class songsNode
 {
 public:
@@ -27,11 +26,14 @@ public:
 	playlist() :tail(nullptr), head(nullptr) {}
 	void addSong(string Song_name, string Artist_name, int Time, string Genre);
 	void display();
-	void removeSongByName();
-	void searchByName(string);
-	//void searchByArtist_name(string);
-	void search(const string& query);
-	void sortSongByName(const string& field);
+	void removeSongByName(const string&);
+	void remove(const string&, const string&);
+	void searchSongByName(string);
+	void search(const string&);
+	void swap(songsNode*,songsNode*);
+	void shuffle();
+	void moveDown(const string&);
+	void moveUp(const string&);
 };
 void playlist::addSong(string Song_name, string Artist_name, int Time, string Genre)
 {
@@ -44,70 +46,204 @@ void playlist::addSong(string Song_name, string Artist_name, int Time, string Ge
 	}
 	else
 	{
-		cout << "Add new song:"<<endl;
+		cout << "Add " << endl;
 		head = tail = newSong;
 	}
 }
-
-//delete ! think about it!
-void playlist::removeSongByName()
+//delete by genre or artist or title
+//first input we put the song name or specific artist name that we want to delete 
+// or specific genre that we want to delete
+//second input is just a key word that we use to find that specific options
+void playlist::remove(const string& word, const string& specific)
 {
-
+	songsNode* temp = head;
+	while (temp) {
+		if ((specific == "N" && temp->Song_name == word) ||
+			(specific == "A" && temp->Artist_name == word) ||
+			(specific == "G" && temp->Genre == word))
+		{
+			if (temp == head)
+			{
+				head = temp->next_song;
+			}
+			if(temp == tail)
+			{
+				tail = temp->previous_song;
+			}
+			if (temp->previous_song)
+			{
+				temp->previous_song->next_song = temp->next_song;
+			}
+			if (temp->next_song)
+			{
+				temp->next_song->previous_song = temp->previous_song;
+			}
+			delete temp;
+			break;
+		}
+		temp = temp->next_song;
+	}
 }
+void playlist::removeSongByName(const string& name)
+{
+	songsNode* temp = head;
+	while (temp)
+	{
+		if (temp->Song_name == name)
+		{
+			// 2 if is for update the pointer when we delete
+			if (temp->previous_song)
+			{
+				temp->previous_song->next_song = temp->next_song;
+			}
+			if (temp->next_song)
+			{
+				temp->next_song->previous_song = temp->previous_song;
+			}
+			//if song to remove is the first in playlist
+			if (temp == head)
+			{
+				head = temp->next_song;
+			}
+			//if song to remove is the last in playlist
+			if (temp == tail)
+			{
+				tail = temp->previous_song;
+			}
+			delete temp;
+			cout << "song name that we remove: " << name << endl;
+			return;
+		}
+		temp = temp->next_song;
+	}
+}
+//search by name or artist or genre
 void playlist::search(const string& query)
 {
+	cout << "Search" << endl;
 	songsNode* temp = head;
 	while (temp)
 	{
 		if (temp->Song_name == query || temp->Artist_name == query || temp->Genre == query)
 		{
-			cout << "Found it! " << temp->Song_name << " by" << temp->Artist_name << endl;
+			cout << "Found it! " << temp->Song_name << " by " << temp->Artist_name << " genre " << temp->Genre << endl;
 		}
 		temp = temp->next_song;
 	}
 }
-void playlist::searchByName(string Song_name)
+//search by only name
+void playlist::searchSongByName(string Song_name)
 {
 	songsNode* temp = head;
+	//bool it's like flag 
 	bool find = false;
 	while (temp)
 	{
 		if (temp->Song_name == Song_name)
 		{
-			cout << "Found it!!!" << temp->Song_name << ", Artist: " << temp->Artist_name 
-				<<", Genre: " << temp->Genre << ", Time: " << temp->Time << " seconds" << endl;
+			cout << "Found it!" << temp->Song_name << " Artist: " << temp->Artist_name
+				<< " Genre: " << temp->Genre << " Time: " << temp->Time << " seconds" << endl;
 			find = true;
 		}
 		temp = temp->next_song;
-		if(find = false)
+		if (find = false)
 		{
 			cout << "Can't found it!" << endl;
 		}
 	}
 }
-void playlist::sortSongByName(const string &field)
+//swap function
+void playlist::swap(songsNode* a, songsNode* b) {
+	string tempTitle = a->Song_name;
+	string tempArtist = a->Artist_name;
+	string tempGenre = a->Genre;
+	int tempDuration = a->Time;
+
+	a->Song_name = b->Song_name;
+	a->Artist_name = b->Artist_name;
+	a->Genre = b->Genre;
+	a->Time = b->Time;
+
+	b->Song_name = tempTitle;
+	b->Artist_name = tempArtist;
+	b->Genre = tempGenre;
+	b->Time = tempDuration;
+}
+void playlist::shuffle()
 {
-	//check the playlist that has a song or 1 song?
-	if (!head || !head->next_song)
+	if (!head)
 	{
-		cout << "The playlist is empty or has 1 song!" << endl;
+		cout << "The list is EMPTY!";
 		return;
 	}
-	bool swapped;
-	do {
-		swapped = false;
-		songsNode* current = head;
-		while (current && current->next_song) {
-			if ((field == "title" && current->Song_name > current->next_song->Song_name) ||
-				(field == "artist" && current->Artist_name > current->next_song->Artist_name)) {
-				swap(current, current->next_song);
-				swapped = true;
-			}
-			current = current->next_song;
-		}
-	} while (swapped);
+	int n = 0;
+	songsNode* current = head;
+	while (current) 
+	{
+		n++;
+		current = current->next_song;
+	}
+	for (int i = 0; i < n; i++) 
+	{
+		//rand function we calculate random number %(residuum) of n
+		int random1 = rand() % n;
+		int random2 = rand() % n;
 
+		songsNode* song1 = head;
+		for (int j = 0; j < random1; j++) 
+		{
+			song1 = song1->next_song;
+		}
+
+		songsNode* song2 = head;
+		for (int j = 0; j < random2; j++) 
+		{
+			song2 = song2->next_song;
+		}
+		swap(song1, song2);
+	}
+	cout << "shuffle" << endl;
 }
+//move down 1 by 1 with this function
+void playlist::moveDown(const string& name) 
+{
+	cout << "Move Down" << endl;
+	if (!head) 
+	{
+		cout << "The playlist is empty." << endl;
+		return;
+	}
+	songsNode* current = head;
+	while (current && current->Song_name != name)
+	{
+		current = current->next_song;
+	}
+	if (current && current->next_song) 
+	{
+		swap(current, current->next_song);
+	}
+}
+void playlist::moveUp(const string& name)
+{
+	cout << "Move Up" << endl;
+	if (!head) 
+	{
+		cout << "The playlist is empty." << endl;
+		return;
+	}
+
+	songsNode* current = tail;
+	while (current && current->Song_name != name) 
+	{
+		current = current->previous_song;
+	}
+
+	if (current && current->previous_song) 
+	{
+		swap(current, current->previous_song);
+	}
+}
+//display the list
 void playlist::display()
 {
 	if (!head)
@@ -116,12 +252,12 @@ void playlist::display()
 		return;
 	}
 	songsNode* current = head;
-	while (current) 
+	while (current)
 	{
-		cout << "Name: " << current->Song_name << endl;
-		cout << "Artist: " << current->Artist_name << endl;
-		cout << "Genre: " << current->Genre << endl;
-		cout<<"Time: " << current->Time << " seconds" << endl;
+		cout << "Name: " << current->Song_name << " "
+			<< "Artist: " << current->Artist_name << " "
+			<< "Genre: " << current->Genre  << " "
+			<< "Time: " << current->Time <<"seconds"<<endl;
 		current = current->next_song;
 	}
 }
@@ -133,10 +269,21 @@ int main()
 	playlist.addSong("The Lonely", "Christina ", 233, "pop");
 	playlist.addSong("Symphony", "Beethoven", 334, "classic");
 	playlist.addSong("Mirror", "Lil Wayne", 228, "rap");
+	playlist.addSong("Je", "Zoz", 219, "jazz");
 	playlist.display();
-	playlist.searchByName("Symphony");
+	playlist.moveDown("Dream");
+	playlist.display();
+	playlist.moveUp("Mirror");
+	playlist.display();
+	playlist.searchSongByName("Symphony");
 	playlist.search("Lil Wayne");
-	cout << "sort !!!!!" << endl;
-	playlist.sortSongByName("Artist_name");
 	playlist.display();
+	playlist.shuffle();
+	playlist.display();
+	playlist.removeSongByName("Mirror");
+	playlist.display();
+	playlist.remove("classic","G");
+	playlist.display();
+	//playlist.remove("Imagine Dragons","A");
+	//playlist.display();
 }
